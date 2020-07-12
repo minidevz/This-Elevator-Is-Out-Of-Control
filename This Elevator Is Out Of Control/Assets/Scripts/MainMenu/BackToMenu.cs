f52c0e50bdff2cb402f5c610ed4e8ed7b6856b98
Ibrahim Hisham <minidevz@users.noreply.github.com>
2020-07-12 21:05:55 +0500

    1.0 Release!
    Finally fucking released this shit, im so fucking tired,

---
+++ This Elevator Is Out Of Control/Assets/Scripts/MainMenu/BackToMenu.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BackToMenu : MonoBehaviour
+{
+    //Ibrahim Hisham, Copyright Minidevs. 2019
+    #region Variables
+
+
+    #endregion
+    private void Start ( )
+    {
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+
+    public void BackToMenuButton()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+}
---
+++ This Elevator Is Out Of Control/Assets/Scripts/Misc/DatabaseDestroyer.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class DatabaseDestroyer : MonoBehaviour
+{
+    //Ibrahim Hisham, Copyright Minidevs. 2019
+    #region Variables
+    public bool inputActive;
+    public Animator destroyDatabase;
+    private int destroyCombo;
+    public TextMeshProUGUI comboText;
+    #endregion
+    private void OnTriggerEnter (Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            inputActive = true;
+            comboText.gameObject.SetActive(true);
+        }
+    }
+    void Update()
+    {
+    
+        if(Input.GetKeyDown(KeyCode.E) && inputActive == true)
+        {
+            destroyCombo++;
+        }
+
+        comboText.text = destroyCombo.ToString() + "/10";
+       
+        if(destroyCombo == 10)
+        {
+            destroyDatabase.SetBool("destroying", true);
+          
+            this.GetComponent<Collider>().enabled = false;
+            comboText.gameObject.SetActive(false);
+            inputActive = false;
+            this.enabled = false;
+        }
+    }
+    private void OnDisable ( )
+    {
+        this.GetComponent<DialogueIncrement>().IncrementDialogue();
+    }
+}
