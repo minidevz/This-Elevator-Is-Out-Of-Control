8fd826a4b5f1b31f97b449e5e60844cf6b58747d
Ibrahim Hisham <minidevz@users.noreply.github.com>
2020-07-12 19:13:35 +0500

    Added Main Menu
    Also added player death and about to make ending.

---
+++ This Elevator Is Out Of Control/Assets/Scripts/EndingTrigger.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndingTrigger : MonoBehaviour
+{
+  //Ibrahim Hisham, Copyright Minidevs. 2019
+   #region Variables
+
+
+    #endregion
+    void Start()
+    {
+        
+    }
+
+    private void OnTriggerEnter (Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            other.GetComponent<Animator>().enabled = true;
+         
+        }
+    }
+
+
+}
---
+++ This Elevator Is Out Of Control/Assets/Scripts/MenuManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class MenuManager : MonoBehaviour
+{
+  //Ibrahim Hisham, Copyright Minidevs. 2019
+   #region Variables
+
+
+    #endregion
+  public void PlayGame()
+    {
+        SceneManager.LoadScene("MainScene");
+    }
+
+    public void QuitGame ( )
+    {
+        Application.Quit();
+    }
+}
